bf4fc1f33e6af967c24c7b4f05e336dfd4536bbf
Ashydiki Malik <budimargono170764@gmail.com>
2024-06-10 08:04:37 +0700

    init

---
+++ chapter4/practice/akbar/main.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    name: 'Jono',
+    email: 'jono@binar.com',
+    password: 'jono1123',
+  },
+];
+
+const userPostgres = new UserRepositoryPostgres(users);
+const userService = new UserService(userPostgresMock);
+
+const userToRegister = {
+  name: 'abby',
+  email: 'abby@binar.com',
+  password: 'abby123',
+};
+
+console.log('[userService] register(): ', JSON.stringify(userService.register(userToRegister)));
+
+const userToLogin = {
+  email: 'abby@binar.com',
+  password: 'abby123',
+};
+
+console.log('[userService] login(): ', JSON.stringify(userService.login(userToLogin)));
---
+++ chapter4/practice/akbar/service/userServiceImport.js
@@ -0,0 +1,23 @@
+const UserRepository = require('../repository/userRepository');
+const ProductService = require('../repository/productService');
+
+class UserService {
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  register({ name, email, password }) {
+    // TODO:
+    // 1. Lakukan penambahan data user kedalam database (melalui class repository)
+    // 2. Pastikan untuk mengecek apakah email sudah terdaftar atau belum, jika sudah terdaftar, kembalikan error
+    // 3. Jika berhasil kembalikan data user yang sudah berhasil ditambahkan
+    this.userRepository.getAll();
+  }
+
+  login({ email, password }) {
+    // TODO:
+    // 1. Lakukan pengecekan ke database (melalui repository)
+    // 2. Ketika email dan password ada dan sesuai, kembalikan pesan "Login berhasil"
+    // 3. Jika tidak kembalikkan error "Email atau password salah"
+  }
+}
